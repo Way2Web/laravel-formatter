b7398f7ebae2b156f13c45546ad0e5bedc86194f
Robert Smagge <robert.smagge@way2web.nl>
2018-01-19 13:05:13 +0100

    Fixed the XMLParser so that parsing from Json to XML and back gives the same result.

--- src/SoapBox/Formatter/Parsers/XmlParser.php
+++ src/SoapBox/Formatter/Parsers/XmlParser.php
@@ -1,36 +1,44 @@
-<?php namespace SoapBox\Formatter\Parsers;
+<?php
 
-class XmlParser extends Parser {
+namespace SoapBox\Formatter\Parsers;
 
-	private $xml;
+class XmlParser extends Parser
+{
+    private $xml;
 
-	/**
-	 * Ported from laravel-formatter
-	 * https://github.com/SoapBox/laravel-formatter
-	 *
-	 * @author  Daniel Berry <daniel@danielberry.me>
-	 * @license MIT License (see LICENSE.readme included in the bundle)
-	 *
-	 */
-	private function objectify($value) {
-		$temp = is_string($value) ?
-			simplexml_load_string($value, 'SimpleXMLElement', LIBXML_NOCDATA) :
-			$value;
+    /**
+     * Ported from laravel-formatter
+     * https://github.com/SoapBox/laravel-formatter.
+     *
+     * @author  Daniel Berry <daniel@danielberry.me>
+     * @license MIT License (see LICENSE.readme included in the bundle)
+     */
+    private function objectify($value)
+    {
+        $temp = is_string($value) ? simplexml_load_string($value, 'SimpleXMLElement', LIBXML_NOCDATA) : $value;
 
-		$result = [];
+        $result = [];
 
-		foreach ((array) $temp as $key => $value) {
-			$result[$key] = (is_array($value) or is_object($value)) ? $this->objectify($value) : $value;
-		}
+        foreach ((array) $temp as $key => $value) {
+            if (is_array($value)) {
+                $result = $this->objectify($value);
+            } elseif (is_object($value)) {
+                $result[$key] = $this->objectify($value);
+            } else {
+                $result[$key] = $value;
+            }
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 
-	public function __construct($data) {
-		$this->xml = $this->objectify($data);
-	}
+    public function __construct($data)
+    {
+        $this->xml = $this->objectify($data);
+    }
 
-	public function toArray() {
-		return (array) $this->xml;
-	}
+    public function toArray()
+    {
+        return (array) $this->xml;
+    }
 }
